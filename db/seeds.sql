57af5d800f758002a4a61fa10401d8262aa71946
Bartok <thom.huenger@gmail.com>
2021-05-03 15:40:58 -0400

    added images and fixed some routing issues

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('');
+INSERT INTO burgers (name) VALUES ('Charlie');
+INSERT INTO burgers (name, eaten) VALUES ('Poppy', true);
+INSERT INTO burgers (name, eaten) VALUES ('Oscar', true);
+INSERT INTO burgers (name, eaten) VALUES ('Smudge', true);
+INSERT INTO burgers (name) VALUES ('Daisy');
