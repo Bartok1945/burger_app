5d2ddf4cc78b7e9108c6d02153a76de9777d833d
Bartok <thom.huenger@gmail.com>
2021-05-03 22:39:11 -0400

    formatting

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const routes = require("./controllers/burgerController.js");
 app.use(routes);
 
 // Start our server to begin listening to client requests
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
